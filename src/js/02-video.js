4120cec4525daafb78eb22456e3aee424ace861b
Tihons <tihonshramenko@gmail.com>
2023-12-10 14:39:23 +0200

    save

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function currentVideoTime() {
+  player.on(
+    'timeupdate',
+    throttle(function (event) {
+      
+      let value = 0;
+      console.log(event.seconds);
+      value += event.seconds;
+
+      localStorage.setItem('videoplayer-current-time', value);
+    }, 1000)
+  );
+}
+currentVideoTime();
+
+if (localStorage.getItem('videoplayer-current-time') !== null) {
+  const currentBreakpoint = localStorage.getItem('videoplayer-current-time');
+  player.setCurrentTime(currentBreakpoint);
+} else {
+  player.setCurrentTime(0);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
+
+function chekInputValue() {
+  
+  const savedData = localStorage.getItem(localStorageKey);
+  const parsedData = JSON.parse(savedData);
+  if (parsedData === null) {
+    return;
+  }
+  form[0].value = parsedData.email;
+  form[1].value = parsedData.message;
+}
+chekInputValue();
+
+form.addEventListener('input', throttle(onInputListener, 500));
+function onInputListener(evt) {
+
+  const enteredData = {
+    email: form.email.value,
+    message: form.message.value.trim(),
+  };
+
+  localStorage.setItem(localStorageKey, JSON.stringify(enteredData));
+  const savedSettings = localStorage.getItem(localStorageKey);
+  const parsedSettings = JSON.parse(savedSettings);
+ 
+}
+
+form.addEventListener('submit', onSubmitListener);
+function onSubmitListener(evt) {
+  
+  evt.preventDefault();
+  const savedData = localStorage.getItem(localStorageKey);
+  const parsedData = JSON.parse(savedData);
+
+ 
+  if (parsedData === null) {
+    return alert('All fields must be filled!');
+  }
+  if (parsedData.email === '' || parsedData.message === '') {
+    return alert('All fields must be filled!');
+  }
+  console.log(parsedData);
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+}
